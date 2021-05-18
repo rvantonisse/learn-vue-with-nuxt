282c2749f071907672c09ca697e75db0fa210a11
Roel Antonisse <roel.antonisse@gmail.com>
2021-05-18 15:41:02 +0200

    feat(homepage): add mock content
    Characters, Planets, Starships

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,30 @@
   <main>
     <h1>{{ film.title }}</h1>
     <p>{{ film.opening_crawl }}</p>
+
+    <h2>Characters</h2>
+    <ul>
+      <li>Leia</li>
+      <li>R2-D2</li>
+      <li>C-3PO</li>
+    </ul>
+    <a href="/characters">See all characters</a>
+
+    <h2>Planets</h2>
+    <ul>
+      <li>Tatooine</li>
+      <li>Alderaan</li>
+      <li>Yavin IV</li>
+    </ul>
+    <a href="/planets">See all planets</a>
+
+    <h2>Starships</h2>
+    <ul>
+      <li>Death Star</li>
+      <li>T-Fighter</li>
+      <li>X-wing</li>
+    </ul>
+    <a href="/starships">See all starships</a>
   </main>
 </template>
 
