8eebba808cb3b598ff3064bf29be18a3faeeda03
Roel Antonisse <roel.antonisse@gmail.com>
2021-05-25 16:10:47 +0200

    feat(homepage): add character contents

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,29 @@
     <h2>Characters</h2>
     <ul>
       <li v-for="character in fetchedData.characters" :key="character.url">
-        {{ character.name }}
+        <base-card>
+          <template #header>
+            <h2>{{ character.name }}</h2>
+          </template>
+          <template #default>
+            <base-character-image
+              :skin-color="character.skin_color"
+              :eye-color="character.eye_color"
+              :height="character.heigth"
+              :width="50 + parseInt(character.mass)"
+            ></base-character-image>
+            <dl>
+              <dt>Skin</dt>
+              <dd>{{ character.skin_color }}</dd>
+
+              <dt>Eye color</dt>
+              <dd>{{ character.eye_color }}</dd>
+
+              <dt>Heigth</dt>
+              <dd>{{ character.height }}</dd>
+            </dl>
+          </template>
+        </base-card>
       </li>
     </ul>
     <a href="/characters">See all characters</a>
