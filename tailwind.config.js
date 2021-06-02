17f85ddfa66d8636d5b3bfa85713a555726eb3bf
Roel Antonisse <roel.antonisse@gmail.com>
2021-06-02 15:33:54 +0200

    feat(tailwind): update theme config with width

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   theme: {
     extend: {
       minWidth: {
+        '10rem': '10rem',
+        '20rem': '20rem',
+      },
+      maxWidth: {
         '20rem': '20rem',
       },
     },
