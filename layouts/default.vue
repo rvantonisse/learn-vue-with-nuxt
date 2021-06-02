31e5b8e6004c4433cd5b471b9df6e0250752d35e
Roel Antonisse <roel.antonisse@gmail.com>
2021-06-02 16:28:24 +0200

    feat(default layouts): add a simple header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,21 @@
 <template>
-  <Nuxt />
+  <div>
+    <header>
+      <nuxt-link to="/">Star Wars: A&nbsp;New Hope</nuxt-link>
+    </header>
+
+    <div id="content">
+      <Nuxt />
+    </div>
+  </div>
 </template>
+
+<style>
+  #content {
+    @apply p-4;
+  }
+
+  header {
+    @apply p-4 text-6xl;
+  }
+</style>
