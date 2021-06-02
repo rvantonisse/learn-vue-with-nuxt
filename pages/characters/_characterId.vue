e0a1e446b23a61ee92bf1cdfc264c2a4018e9a99
Roel Antonisse <roel.antonisse@gmail.com>
2021-06-02 15:35:02 +0200

    feat(pages): add dynamic character detail page

---
+++ pages/characters/_characterId.vue
@@ -0,0 +1,20 @@
+<template>
+  <base-page :page-title="character.name">
+    <base-character-image
+      :skin-color="character.skin_color"
+      :eye-color="character.eye_color"
+      :height="character.height"
+    ></base-character-image>
+  </base-page>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params, $http }) {
+      const id = params.characterId
+      const character = await $http.$get(`https://swapi.dev/api/people/${id}`)
+
+      return { character }
+    },
+  }
+</script>
