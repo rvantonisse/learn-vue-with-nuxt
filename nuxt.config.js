5c1d4010b74a3e4c9fa681068ac095abf4f8388b
Roel Antonisse <roel.antonisse@gmail.com>
2021-06-02 16:41:57 +0200

    chore(nuxt): update head.title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-vue-with-nuxt',
+    title: 'Star Wars: A New Hope',
     htmlAttrs: {
       lang: 'en',
     },
