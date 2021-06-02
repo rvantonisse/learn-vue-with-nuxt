73841771284a324a66c2b978f118bce58501ac9c
Roel Antonisse <roel.antonisse@gmail.com>
2021-06-02 16:48:26 +0200

    feat(homepage): add iteration over planet and starship data

--- pages/index.vue
+++ pages/index.vue
@@ -17,21 +17,25 @@
       <nuxt-link to="/characters">See all characters</nuxt-link>
     </article>
 
-    <h2>Planets</h2>
-    <ul>
-      <li>Tatooine</li>
-      <li>Alderaan</li>
-      <li>Yavin IV</li>
-    </ul>
-    <a href="/planets">See all planets</a>
+    <article>
+      <h2>Planets</h2>
+      <p>Planets visited:</p>
+      <ul>
+        <li v-for="planet in fetchedData.planets" :key="planet.url">
+          {{ planet.name }}
+        </li>
+      </ul>
+    </article>
 
-    <h2>Starships</h2>
-    <ul>
-      <li>Death Star</li>
-      <li>T-Fighter</li>
-      <li>X-wing</li>
-    </ul>
-    <a href="/starships">See all starships</a>
+    <article>
+      <h2>Starships</h2>
+      <p>Starships seen:</p>
+      <ul>
+        <li v-for="starship in fetchedData.starships" :key="starship.url">
+          {{ starship.name }}
+        </li>
+      </ul>
+    </article>
   </base-page>
 </template>
 
